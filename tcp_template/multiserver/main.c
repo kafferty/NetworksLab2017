08d939e63017579543f2d72067ea8a5b910f3a03
Lidia <gorodnicheva.lidia@yandex.ru>
2017-10-01 18:17:55 +0300

    multiserver

--- tcp_template/multiserver/main.c
+++ tcp_template/multiserver/main.c
@@ -1,29 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <winsock2.h>
-#include <stdint.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
 #include <string.h>
 #include "pthread.h"
-
-void* formulti (void* temp) {
-	int n = *((int*)temp);
-	char buffer[256];
-	char *p = buffer;
-	while (1) {
-		memset(buffer, 0, 256);
-		int k = readn (n, p, 255);
-		if (k > 0) {
-			printf("%s \n", buffer);
-			send(n, "I have got your message", 23, 0);
-		}
-	}
-}
 
 int readn(int sockfd, char *buf, int n){
     int k;
     int off = 0;
     for(int i = 0; i < n; ++i){
-        k = recv(sockfd, buf + off, 1, 0);
+        k = read(sockfd, buf + off, 1);
         off += 1;
         if (k < 0){
             printf("Error. reading from socket \n");
@@ -33,12 +21,27 @@ int readn(int sockfd, char *buf, int n){
     return off;
 }
 
-int main(int argc, char *argv[]) {
+void* formulti (void* temp) {
+	int n = *((int*)temp);
+	char buffer[256];
+	char *p = buffer;
+		bzero(buffer, 256);
+		int k = readn (n, p, 255);
+		if (k > 0) {
+			printf("%s \n", buffer);
+			write(n, "I have got your message", 23);
+		}
+    shutdown(n, 2);
+    close(n);
+}
+
+
+
+int main() {
     int sockfd, newsockfd;
     uint16_t portno;
     unsigned int clilen;
     char buffer[256];
-    char *p = buffer;
     struct sockaddr_in serv_addr, cli_addr;
     ssize_t n;
 
@@ -51,7 +54,7 @@ int main(int argc, char *argv[]) {
     }
 
     /* Initialize socket structure */
-    memset((char *) &serv_addr, 0, sizeof(serv_addr));
+    bzero((char *) &serv_addr, sizeof(serv_addr));
     portno = 5001;
 
     serv_addr.sin_family = AF_INET;
@@ -68,42 +71,25 @@ int main(int argc, char *argv[]) {
        * go in sleep mode and will wait for the incoming connection
     */
     while (1) {
-    listen(sockfd, 5);
-    clilen = sizeof(cli_addr);
+        listen(sockfd, 5);
+        clilen = sizeof(cli_addr);
 
-    /* Accept actual connection from the client */
-    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+        /* Accept actual connection from the client */
+        newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
 
-    shutdown(sockfd, 2);
-    closesocket(sockfd);
-    
-    if (newsockfd < 0) {
-        perror("ERROR on accept");
-        exit(1);
+
+        if (newsockfd < 0) {
+            perror("ERROR on accept");
+            exit(1);
+        }
+
+
+        pthread_t tid;
+        pthread_attr_t attr;
+        pthread_attr_init(&attr);
+        pthread_create(&tid, &attr, formulti, &newsockfd);
+        pthread_detach(tid);
     }
-
-    /* If connection is established then start communicating */
-    //bzero(buffer, 256);
-
-    /*n = readn(newsockfd, p, 255);
-
-    printf("Here is the message: %s\n", buffer);
-
-
-    n = write(newsockfd, "I got your message", 18); // send on Windows
-
-    if (n < 0) {
-        perror("ERROR writing to socket");
-        exit(1);
-    } */    
-	pthread_t tid;
-	pthread_attr_t attr;
-	pthread_attr_init(&attr);
-	pthread_create(&tid, &attr, formulti, &newsockfd);
-	pthread_detach(tid);
-}
-    shutdown(newsockfd, 2);
-    closesocket(newsockfd);
 
     return 0;
 }
